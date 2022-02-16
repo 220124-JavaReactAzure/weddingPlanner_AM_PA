e99790b0e1eb5517ab5420b972d2aed23d757b73
Austin07151 <94205817+AGameZzz01@users.noreply.github.com>
2022-02-16 16:50:21 -0600

    Prepared User for testing

--- Project1/src/main/java/com/revature/weddingPlans/web/servlets/UserServlet.java
+++ Project1/src/main/java/com/revature/weddingPlans/web/servlets/UserServlet.java
@@ -2,17 +2,23 @@ package com.revature.weddingPlans.web.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.weddingPlans.models.User;
 import com.revature.weddingPlans.services.UserServices;
 
-public class UserServlet extends HttpServlet {
+
+
+
+public class UserServlet extends HttpServlet{
 	
 	private final UserServices userServices;
 	private final ObjectMapper mapper;
@@ -22,9 +28,12 @@ public class UserServlet extends HttpServlet {
 		this.mapper = mapper;
 	}
 	
+	// RCUD - order
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// Switch statements are back sorry
 		PrintWriter writer = resp.getWriter();
+		// Obtains everything after the /users
 		String path = req.getPathInfo();
 		if(path == null) path = "";
 		switch(path) {
@@ -33,17 +42,78 @@ public class UserServlet extends HttpServlet {
 				String idParam = req.getParameter("userId");
 				if(idParam == null) {
 					resp.setStatus(400);
-					writer.write("Please include the ?userId=# in your url");
+					writer.write("Please include the query ?userId=# in your url");
 					return;
 				}
 				
 				int userId = Integer.valueOf(idParam);
 				
+			
 				User user = userServices.getUserById(userId);
-			} catch (Exception e) {
-				// TODO: handle exception
+				if(user == null) {
+					resp.setStatus(500);
+					return;
+				}
+				String payload = mapper.writeValueAsString(user);
+				writer.write(payload);
+				resp.setStatus(200);
+			} catch (StreamReadException | DatabindException e) {
+				resp.setStatus(400);
 			}
+			break;
+		default:
+			List<User> users = userServices.getAllUsers();
+			String payload = mapper.writeValueAsString(users);
+			writer.write(payload);
+			resp.setStatus(200);
+			break;
 		}
 	}
-
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("application/json");
+		try {
+			User newuser = mapper.readValue(req.getInputStream(), User.class);
+			boolean wasReg = userServices.addUser(newuser);
+			if(wasReg) {
+				resp.setStatus(201);
+			} else {
+				resp.setStatus(500);
+				resp.getWriter().write("Database did not persist");
+			}
+		} catch (StreamReadException | DatabindException e) {
+			resp.setStatus(400);
+			resp.getWriter().write("JSON threw exception");
+			e.printStackTrace();
+		} catch (Exception e) {
+			resp.setStatus(500);
+			resp.getWriter().write("Some other random exception did not persist");
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			User updateduser = mapper.readValue(req.getInputStream(), User.class);
+			userServices.updateUserWithSessionMethod(updateduser);
+			resp.setStatus(204);	
+		} catch (StreamReadException | DatabindException e) {
+			resp.setStatus(400);
+			resp.getWriter().write("JSON threw exception");
+			e.printStackTrace();
+		} catch (Exception e) {
+			resp.setStatus(500);
+			resp.getWriter().write("Some other random exception did not persist");
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doDelete(req, resp);
+	}
+	
 }
