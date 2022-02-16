2832faa33f4f697afa97f050acff3f204b31b2b4
Paul Aglipay <PAglipay@ad.it.ucla.edu>
2022-02-16 13:44:29 -0800

    Added UserService

---
+++ Project1/src/main/java/com/revature/weddingPlans/services/UserServices.java
@@ -0,0 +1,43 @@
+package com.revature.weddingPlans.services;
+
+import java.util.List;
+
+import com.revature.weddingPlans.models.User;
+
+public class UserServices {
+
+private final UserDAO userDAO;
+	
+	public UserServices(UserDAO userDAO) {
+		this.userDAO = userDAO;
+	}
+	
+	public boolean addUser(User user) {
+		
+		return userDAO.addUser(user);
+	}
+	
+	public List<User> getAllUsers(){
+		return userDAO.getAllUsers();
+		
+	}
+	
+	public User getUserById(int id){
+		
+		return userDAO.getUserById(id);
+	}
+	
+	
+	public void updateUserWithSessionMethod(User user) {
+		
+		userDAO.updateUserWithSessionMethod(User);
+	}
+	
+	public void updateUserWithHQL(User user) {
+		
+		userDAO.updateUserWithHQL(user);
+	}
+	
+	
+}
+
