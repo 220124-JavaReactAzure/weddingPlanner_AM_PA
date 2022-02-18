813eea9d8b7b4d216772409f82d42dc7c828bf8d
Paul Aglipay <PAglipay@ad.it.ucla.edu>
2022-02-18 08:27:27 -0800

    Created Service Model

---
+++ Project1/src/main/java/com/revature/weddingPlans/models/Services.java
@@ -0,0 +1,112 @@
+package com.revature.weddingPlans.models;
+
+import java.util.Objects;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+public class Services {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="service_id")
+	private int serviceId;
+	
+
+	@Column(name="service_name")
+	private String serviceName;
+	
+
+	@Column(name="service_cost")
+	private double serviceCost;
+	
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@Column(name="service_type_id")
+	private int serviceType;
+	
+	
+
+	public Services() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Services(String serviceName, double serviceCost, int serviceType) {
+		super();
+		this.serviceName = serviceName;
+		this.serviceCost = serviceCost;
+		this.serviceType = serviceType;
+	}
+
+	public Services(int serviceId, String serviceName, double serviceCost, int serviceType) {
+		super();
+		this.serviceId = serviceId;
+		this.serviceName = serviceName;
+		this.serviceCost = serviceCost;
+		this.serviceType = serviceType;
+	}
+
+	public int getServiceId() {
+		return serviceId;
+	}
+
+	public void setServiceId(int serviceId) {
+		this.serviceId = serviceId;
+	}
+
+	public String getServiceName() {
+		return serviceName;
+	}
+
+	public void setServiceName(String serviceName) {
+		this.serviceName = serviceName;
+	}
+
+	public double getServiceCost() {
+		return serviceCost;
+	}
+
+	public void setServiceCost(double serviceCost) {
+		this.serviceCost = serviceCost;
+	}
+
+	public int getServiceType() {
+		return serviceType;
+	}
+
+	public void setServiceType(int serviceType) {
+		this.serviceType = serviceType;
+	}
+
+	@Override
+	public String toString() {
+		return "Services [serviceId=" + serviceId + ", serviceName=" + serviceName + ", serviceCost=" + serviceCost
+				+ ", serviceType=" + serviceType + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(serviceCost, serviceId, serviceName, serviceType);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Services other = (Services) obj;
+		return Double.doubleToLongBits(serviceCost) == Double.doubleToLongBits(other.serviceCost)
+				&& serviceId == other.serviceId && Objects.equals(serviceName, other.serviceName)
+				&& serviceType == other.serviceType;
+	}
+	
+	
+}
