73c4b8d2858e92f9a52715a0f014f71025d6361b
AGameZzz01 <agamezamyt@gmail.com>
2022-02-24 13:35:48 -0600

    Tests for Employee added

---
+++ Project1/src/test/java/com/revature/weddingPlans/models/GuestTest.java
@@ -0,0 +1,14 @@
+package com.revature.weddingPlans.models;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GuestTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
